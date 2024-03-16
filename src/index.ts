8446a5550f39a3f894103a944b87decf2a73b5a2
飞小RAN <55832499+xiaofeiTM233@users.noreply.github.com>
2024-03-16 17:58:18 +0800

    index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,23 @@
-import { serve } from '@hono/node-server';
-import app from './app';
+import { Hono } from 'hono';
+import { logger } from 'hono/logger';
+import { getRuntimeKey } from 'hono/adapter';
+import { index } from './middleware';
+import { illust } from './middleware/illust';
+import { pixivReverseProxy } from './utils/pixiv';
+import { env } from './utils/env';
+import { handle } from 'hono/vercel'
 
-serve(app);
+export const config = {
+  runtime: 'edge'
+}
+
+const app = new Hono({ strict: false });
+
+app.use('*', logger());
+app.get('/', index);
+app.get('/favicon.ico', c => c.notFound());
+app.get('/:pid{\\d+}/:p{\\d+}?', illust);
+app.get('/:size/:pid{\\d+}/:p{\\d+}?', illust);
+app.get('/*', c => pixivReverseProxy(c));
+
+export default handle(app)
