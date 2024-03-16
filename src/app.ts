48bb8d57825d5287a660d8324fcd3ff9e16c969d
飞小RAN <55832499+xiaofeiTM233@users.noreply.github.com>
2024-03-16 17:56:19 +0800

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,11 @@ import { index } from './middleware';
 import { illust } from './middleware/illust';
 import { pixivReverseProxy } from './utils/pixiv';
 import { env } from './utils/env';
+import { handle } from 'hono/vercel'
+
+export const config = {
+  runtime: 'edge'
+}
 
 const app = new Hono({ strict: false });
 
@@ -15,13 +20,4 @@ app.get('/:pid{\\d+}/:p{\\d+}?', illust);
 app.get('/:size/:pid{\\d+}/:p{\\d+}?', illust);
 app.get('/*', c => pixivReverseProxy(c));
 
-const port = Number(env.PORT) || 8080;
-
-if (['node', 'deno', 'bun'].includes(getRuntimeKey())) {
-  console.log(`[${new Date().toLocaleString()}] server is running at http://localhost:${port}`);
-}
-
-export default {
-  port,
-  fetch: app.fetch,
-};
+export default handle(app)
