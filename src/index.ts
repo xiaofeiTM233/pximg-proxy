73cfb84bc18bd64b000d0fb4f6a1581c8e0ca8de
飞小RAN <55832499+xiaofeiTM233@users.noreply.github.com>
2024-03-16 18:08:40 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,13 @@ import { index } from './middleware';
 import { illust } from './middleware/illust';
 import { pixivReverseProxy } from './utils/pixiv';
 import { env } from './utils/env';
-import { handle } from 'hono/vercel'
+import { handle } from 'hono/vercel';
 
 export const config = {
   runtime: 'edge'
-}
+};
 
-const app = new Hono({ strict: false });
+const app = new Hono().basePath("/api");
 
 app.use('*', logger());
 app.get('/', index);
@@ -19,5 +19,10 @@ app.get('/favicon.ico', c => c.notFound());
 app.get('/:pid{\\d+}/:p{\\d+}?', illust);
 app.get('/:size/:pid{\\d+}/:p{\\d+}?', illust);
 app.get('/*', c => pixivReverseProxy(c));
+app.get('/hello', (c) => {
+  return c.json({
+    message: 'Hello Next.js!',
+  })
+})
 
 export default handle(app)
